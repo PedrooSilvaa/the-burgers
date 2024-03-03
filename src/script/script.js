e93e2560a0ced020f144fccd867bb01ede7702ec
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-02 22:45:25 -0300

    feat: changes in filter

---
+++ src/script/script.js
@@ -0,0 +1,14 @@
+function Filter(){
+    document.getElementById("list-filter").style.display = "flex";
+}
+
+function Apply(){
+    document.getElementById("list-filter").style.display = "none";
+}
+
+function CloseMeat(){
+    
+}
+function Close
+function Close
+function Close
